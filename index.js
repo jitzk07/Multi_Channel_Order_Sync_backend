c9cff7e50d760d1c6fda0acc4a54f02421406b5b
Jitendra Kumar <115620278+jitzk07@users.noreply.github.com>
2025-06-19 17:47:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173','https://multi-channel.netlify.app']
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
